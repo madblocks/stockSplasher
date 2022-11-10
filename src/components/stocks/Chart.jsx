544b6c2b2d1a774aea4678b785dd1c65fd267b6f
Andrew Kennedy <akennedy811@icloud.com>
2022-11-09 23:28:07 -0500

    changed chart api to use https

--- src/components/stocks/Chart.jsx
+++ src/components/stocks/Chart.jsx
@@ -18,7 +18,7 @@ export default function Chart ({stock, isActive}) {
     <StyledChartDisplay active={isActive}>
       <div className='chart'>
         <iframe frameBorder='0' scrolling='no' width='800' height='420' title='chart'
-          src={`http://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=8961019C3F42432E927E3C9EECFCC103&symbol=${stock.symbol}&days=365&width=800&height=420`}>
+          src={`https://api.stockdio.com/visualization/financial/charts/v1/HistoricalPrices?app-key=8961019C3F42432E927E3C9EECFCC103&symbol=${stock.symbol}&days=365&width=800&height=420`}>
         </iframe>
       </div>
     </StyledChartDisplay>
